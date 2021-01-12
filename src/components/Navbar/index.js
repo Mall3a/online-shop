8b7eabc2f6bcf04c78e49c5607c8f6711781379b
Constanza Mallea <constanza.mallea@grouponlatam.com>
2021-01-12 18:09:02 -0300

    Changes in navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
-
+import logo from '../../images/logo.png'
 import styles from './index.module.css';
 
 const Navbar = props => {
     return (
         <nav className={styles.navbar}>
-            <h3>Logo</h3>
+            <Link to="/">
+                <img className={styles.logo} src={logo} />
+            </Link>
             <ul className={styles.navLinks}>
                 <Link className={styles.link} to="/shop">
                     <li>Shop</li>
